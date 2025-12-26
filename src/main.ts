ea00f20e281e4a9f42173e400587bc4394f36bb6
Miguel Pujazón <mpujazoncardenas@gmail.com>
2025-12-26 01:26:25 +0100

    Fetch Weather from UI done

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
-import type { WeatherData } from "./scripts/types";
 import { fetchWeather } from "./services/api";
 
-const weather: WeatherData = await fetchWeather('sant celoni');
-console.log(weather.main.temp);
+const handleSubmit = async ()=> {
+    const searchTerm = cityInput?.value.trim();
+
+    if(!searchTerm) return;
+    
+    try{
+        const weatherDetails = await fetchWeather(searchTerm);
+        console.log(`
+            Humidity: ${weatherDetails.main.humidity}
+            Temperature: ${weatherDetails.main.temp}
+            Country Code: ${weatherDetails.sys.country}
+            Weather Description: ${weatherDetails.weather[0].description}
+            Weatcher Icon: ${weatherDetails.weather[0].icon}
+            Wind Speed: ${weatherDetails.wind.speed}
+            `);
+    }catch(error){
+        console.error('Error: ', error);
+        
+    }
+    
+}
+
+const cityInput = document.querySelector<HTMLInputElement>('#city-input');
+
+const discoverBtn = document.getElementById('discover-btn');
+
+discoverBtn?.addEventListener('click', handleSubmit);
+
