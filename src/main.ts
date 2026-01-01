379c4b263def6cf54354e15d19762685435aea96
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-01-02 00:00:26 +0100

    loadRandomCity() Function Implemented

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
+import { cities } from "./scripts/data/data";
 import { fetchCountryDetails } from "./scripts/services/countryAPI";
 import { fetchCityPicture } from "./scripts/services/unsplashAPI";
 import { fetchWeather } from "./scripts/services/weatherAPI";
 import { renderCountryDetails, renderPicture, renderWeatherDetails } from "./scripts/ui";
 
-const handleSubmit = async ()=> {
-    const searchTerm = cityInput?.value.trim();
+const loadCity = async (givenCity?: string)=> {
+    let cityName: string | undefined;
+    givenCity?
+        cityName = givenCity
+        : cityName = cityInput?.value.trim();
+    
 
-    if(!searchTerm) return;
+    if(!cityName) return;
     
     try{
-        const weatherDetails = await fetchWeather(searchTerm);
+        const weatherDetails = await fetchWeather(cityName);
 
         const [countryDetails, cityPicture] = await Promise.all([
             fetchCountryDetails(weatherDetails.sys.country),
-            fetchCityPicture(searchTerm)
+            fetchCityPicture(cityName)
         ]);
         renderWeatherDetails(weatherDetails);
         renderCountryDetails(countryDetails[0], weatherDetails.name);
@@ -21,12 +26,19 @@ const handleSubmit = async ()=> {
     }catch(error){
         console.error('Error: ', error);
     }
-    
+}
+
+const loadRandomCity = () => {
+    const randomIndex = Math.floor(Math.random()*(cities.length-1));
+    loadCity(cities[randomIndex]);
 }
 
 const cityInput = document.querySelector<HTMLInputElement>('#city-input');
 
 const discoverBtn = document.getElementById('discover-btn');
+discoverBtn?.addEventListener('click', ()=> loadCity());
 
-discoverBtn?.addEventListener('click', handleSubmit);
+const surpriseMeBtn = document.getElementById('random-city-btn');
+surpriseMeBtn?.addEventListener('click', loadRandomCity);
 
+document.addEventListener('DOMContentLoaded', loadRandomCity);
